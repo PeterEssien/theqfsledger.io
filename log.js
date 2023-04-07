542c3f7ebda21cfe29f0f26913962d43ba13f791
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-04-07 08:45:07 +0100

    Update log.js

--- log.js
+++ log.js
@@ -5,18 +5,18 @@ const imgEmail = document.querySelector(".img-email")
 const imgPassword = document.querySelector(".img-password")
 
 document.body.onload = mudaIcon(imgEmail, "./assets/mail-focus.svg")
-inputEmail.addEventListener("focus", () => mudaIcon(imgEmail, "./assets/mail-focus.svg"))
-inputEmail.addEventListener('focusout', () => mudaIcon(imgEmail, "./assets/mail.svg"));
-inputPassword.addEventListener("focus", () => mudaIcon(imgPassword, "./assets/lock-focus.svg"))
-inputPassword.addEventListener("focusout", () => mudaIcon(imgPassword, "./assets/lock.svg"))
+inputEmail.addEventListener("focus", () => mudaIcon(imgEmail, "https://raw.githubusercontent.com/danieldemoura/login-form-css/main/assets/mail-focus.svg"))
+inputEmail.addEventListener('focusout', () => mudaIcon(imgEmail, "https://raw.githubusercontent.com/danieldemoura/login-form-css/main/assets/mail.svg"));
+inputPassword.addEventListener("focus", () => mudaIcon(imgPassword, "https://raw.githubusercontent.com/danieldemoura/login-form-css/main/assets/lock-focus.svg"))
+inputPassword.addEventListener("focusout", () => mudaIcon(imgPassword, "https://raw.githubusercontent.com/danieldemoura/login-form-css/main/assets/lock.svg"))
 buttonSenha.addEventListener("click", mostrarEOcultarSenha)
 
 function mostrarEOcultarSenha() {
     if(inputPassword.type === "password") {
-        buttonSenha.setAttribute("src", "./assets/eye-off.svg")
+        buttonSenha.setAttribute("src", "https://raw.githubusercontent.com/danieldemoura/login-form-css/main/assets/eye-off.svg")
         inputPassword.type = "text"   
     } else {
-        buttonSenha.setAttribute("src", "./assets/eye-on.svg")
+        buttonSenha.setAttribute("src", "https://raw.githubusercontent.com/danieldemoura/login-form-css/main/assets/eye-on.svg")
         inputPassword.type = "password"
     }
 }
