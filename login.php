dfb08fd495b4664bf782760e9af3043aa08ec1ee
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-04-13 17:30:51 +0100

    Update login.php

--- login.php
+++ login.php
@@ -213,5 +213,9 @@ and password='$password'";
 	<script src="assets/js/jquery.min.js"></script>
 		<!--Start of Tawk.to Script-->
 <script type="text/javascript">var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/63d2a7ffc2f1ac1e202fc1cf/1gnne0gp0';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();</script>
-<!--End of Tawk.to Script--></body>
+<!--End of Tawk.to Script-->
+	
+	
+<?php } ?>
+	</body>
 </html>
