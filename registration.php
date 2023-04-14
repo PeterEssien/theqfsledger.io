41c3c793d80ecb5863a8e97fea6fc719cb5e44ce
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-04-14 23:20:37 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -71,17 +71,17 @@ require('db.php');
 // If form submitted, insert values into the database.
 
 if (isset($_REQUEST['email'])){
-        // removes backslashes
+  $name = stripslashes($_REQUEST['name']);
+  $name = mysqli_real_escape_string($con,$name); 
   $username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
   $username = mysqli_real_escape_string($con,$username); 
   $email = stripslashes($_REQUEST['email']);
   $email = mysqli_real_escape_string($con,$email);
   $password = stripslashes($_REQUEST['password']);
   $password = mysqli_real_escape_string($con,$password);
   $trn_date = date("Y-m-d H:i:s");
-        $query = "INSERT into `users` (username, password, email, trn_date)
-VALUES ('$username', '$password', '$email', '$trn_date')";
+        $query = "INSERT into `users` (name, username, password, email, trn_date)
+VALUES ('$name', '$username', '$password', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
         if($result){
             echo "<div class='form'> <h3>You are registered successfully.</h3>
