2c044c68e06a5c34f77fe87b5de825694ca95651
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-04-14 23:24:58 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("exz.h.filess.io","EmergencyDB_cottonwind","9788e03af92da66a15846e459125cb88fa45b393","EmergencyDB_cottonwind");
+$con = mysqli_connect("u7g.h.filess.io,"qfsledger_represent","0897e61b2410a5872b00e9340350e485857b05de","qfsledger_represent");
 // Check connection
 if (mysqli_connect_errno())
   {
