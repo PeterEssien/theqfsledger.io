e5db2fed49aaa34a08084fcbf1ac963a2a371197
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-04-15 01:48:00 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("u7g.h.filess.io,"qfsledger_represent","0897e61b2410a5872b00e9340350e485857b05de","qfsledger_represent");
+$con = mysqli_connect("u7g.h.filess.io","qfsledger_represent","0897e61b2410a5872b00e9340350e485857b05de","qfsledger_represent");
 // Check connection
 if (mysqli_connect_errno())
   {
