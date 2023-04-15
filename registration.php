23bb57a04e8d0fe8dbfac4ede92f2a18597361c2
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-04-15 12:49:32 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -22,7 +22,7 @@
 	<link rel="stylesheet" href="assets/css/others.css">
 	<script src="assets/js/language.js"></script>
 	<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2"></script>
-	<script src='https://www.google.com/recaptcha/api.js' async defer></script>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
 	<script src="assets/javascript/countries.js"></script>
 	<style>
 	html { overflow-y: scroll; overflow-x:hidden; }
@@ -175,7 +175,7 @@ VALUES ('$name', '$username', '$password', '$email', '$trn_date')";
 		</div>
 		<div class="section mb-5 p-2" style="height:100%;overflow-y:auto">
 
-			<form method="post">
+			<form method="post" action="verify.php">
 				<input type="hidden" name="upline" value="No Referral">
 				<div class="card">
 					<div class="card-body pb-1">
@@ -214,7 +214,7 @@ VALUES ('$name', '$username', '$password', '$email', '$trn_date')";
 			
 						<div class=" ">
 							<div class="input-wrapper">
-								<div class="g-recaptcha" data-sitekey="6LcodSwkAAAAAFGhRjMQSGlEeeylny-hpXmOACHu"></div>
+								<div class="g-recaptcha" data-sitekey="6Lf434wlAAAAAK7GvVKRGNc-RLuC7pqFOy0lmmVh"></div>
 							</div>
 						</div>
 				
