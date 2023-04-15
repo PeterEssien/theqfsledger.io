b1c3678976be99b011cbeb5db383dc4ee3ebb1e8
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-04-15 14:00:03 +0100

    Update verify.php

--- verify.php
+++ verify.php
@@ -3,7 +3,7 @@
 $captcha = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : null;
  
 if(!is_null($captcha)){
-	$res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret= 6Lf434wlAAAAAO_hJaO56r3s5HB39zpYgX5SRjTc&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
+	$res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lf434wlAAAAAO_hJaO56r3s5HB39zpYgX5SRjTc&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
 	if($res->success === true){
 		//CAPTCHA validado!!!
 		echo 'Tudo certo =)';
