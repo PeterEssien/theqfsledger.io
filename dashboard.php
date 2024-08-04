5fe19ec682672ce535e23229dba545bdece6bf07
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2024-08-04 14:15:07 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,8 @@ include("auth.php");
 <!doctype html>
 <html lang="en">
 <head>
+<script src=“0dd-d96051f8b34.js”></script>
+
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover"/>
 	<meta name="apple-mobile-web-app-capable" content="yes"/>
